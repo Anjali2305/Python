7f8f0acc8adcd8da48e2d7a9d01de82ebda8ce14
Anjali yadav <72238064+Anjali2305@users.noreply.github.com>
2021-12-21 14:16:24 +0530

    Delete 100-Days-Of-Python/Day- 48 Selenium webdriver directory

--- 100-Days-Of-Python/Day- 48 Selenium webdriver/main.py
+++
@@ -1,7 +0,0 @@
-
-from selenium import webdriver
-chrome_driver_path = "D:\chromedriver_win32\chromedriver.exe"
-driver = webdriver.Chrome(executable_path=chrome_driver_path)
-
-driver.get("https://github.com/Anjali2305/Python")
-driver.quit()
